5d9c06e4a22fe414e4f06babfa1176628feefa95
AngryP1geon <dsh-nsk@yandex.ru>
2026-05-05 19:31:10 +0300

    добавил: реализацию класса страницы товара методы класса страницы товара для теста проверки добавления в корзину и сам тест специфичные локаторы метод решения паззла код актуален на момент решения задачи про независимость контента с поиском бага среди ссылок с разными промокодами включительно

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,12 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form1")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form1")
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages > div:nth-child(1)")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main > h1")
+    PRODUCT_NAME_IN_SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
+    BASKET_COST_MESSAGE = (By.CSS_SELECTOR, "#messages > div:nth-child(3)")
+    PRODUCT_COST = (By.CSS_SELECTOR, ".product_main > p.price_color")
+    PRODUCT_COST_IN_COST_MESSAGE = (By.CSS_SELECTOR, "#messages > div:nth-child(3) > div > p:nth-child(1) > strong")
